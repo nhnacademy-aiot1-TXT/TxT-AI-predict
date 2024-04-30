5aab28eaceed8faa44232fefb83c1fb27dfa7b69
{joohyeon98} <{ljh221803@gmail.com}>
2024-05-01 05:30:08 +0900

    Chore: 버전 수정, model 불러오기 수정

--- predict.py
+++ predict.py
@@ -12,31 +12,42 @@ import schedule
 import pandas as pd
 from datetime import datetime
 
-def get_token(auth_url):
-    """
-    인증 URL에서 토큰 가져오는 함수
+#환경변수 설정
+TENANT_ID = os.environ.get('TENANT_ID')
+USERNAME = os.environ.get('USERNAME')
+PASSWORD = os.environ.get('PASSWORD')
+STORAGE_URL = os.environ.get('STORAGE_URL')
+CONTAINER_NAME = 'TxT-model'
+OBJECT_NAME = 'air_conditional_ai_model.joblib'
+AUTH_URL = 'https://api-identity-infrastructure.nhncloudservice.com/v2.0'
+RABBITMQ_USER = os.environ.get('RABBITMQ_USER')
+RABBITMQ_PASSWORD = os.environ.get('RABBITMQ_PASSWORD')
+RABBITMQ_HOST = os.environ.get('RABBITMQ_HOST')
 
-    Returns:
-    - dict: 토큰을 포함한 JSON 응답
-    """
-    tenant_id = os.environ.get('TENANT_ID')
-    username = os.environ.get('USERNAME')
-    password = os.environ.get('PASSWORD')
+class Auth:
+    @staticmethod
+    def get_token():
+        """
+        인증 URL에서 토큰 가져오는 함수
 
-    token_url = auth_url + '/tokens'
-    req_header = {'Content-Type': 'application/json'}
-    req_body = {
-        'auth': {
-            'tenantId': tenant_id,
-            'passwordCredentials': {
-                'username': username,
-                'password': password
+        Returns:
+        - dict: 토큰을 포함한 JSON 응답
+        """
+
+        token_url = AUTH_URL + '/tokens'
+        req_header = {'Content-Type': 'application/json'}
+        req_body = {
+            'auth': {
+                'tenantId': TENANT_ID,
+                'passwordCredentials': {
+                    'username': USERNAME,
+                    'password': PASSWORD
+                }
             }
         }
-    }
 
-    response = requests.post(token_url, headers=req_header, json=req_body)
-    return response.json()
+        response = requests.post(token_url, headers=req_header, json=req_body)
+        return response.json()
 
 class ObjectService:
     def __init__(self, storage_url, token_id):
@@ -76,42 +87,38 @@ class ObjectService:
         req_url = self._get_url(container, object)
         req_header = self._get_request_header()
         response = requests.get(req_url, headers=req_header)
+        response.raise_for_status()
+
         model_bytes = io.BytesIO(response.content)
         return joblib.load(model_bytes)
 
-def load_model_job():
-    """
-    모델 로드 함수, 매일 자정에 실행
-    """
-    print("새 모델 로드...")
-    AUTH_URL = 'https://api-identity-infrastructure.nhncloudservice.com/v2.0'
-    token = get_token(AUTH_URL)
-    TOKEN_ID = token['access']['token']['id']
+class LoadModelManager():
+    model = None
 
-    STORAGE_URL = os.environ.get('STORAGE_URL')
-    CONTAINER_NAME = 'TxT-model'
-    OBJECT_NAME = 'air_conditional_ai_model.joblib'
-    obj_service = ObjectService(STORAGE_URL, TOKEN_ID)
-    model = obj_service.load_model(CONTAINER_NAME, OBJECT_NAME)
-    return model
+    @staticmethod
+    def load_model_job():
+        """
+        토큰 가져와서 모델 로드, 매일 00시 30에 실행
+        """
+        print("새 모델 로드...")
+        token = Auth.get_token()
+        TOKEN_ID = token['access']['token']['id']
+        
+        obj_service = ObjectService(STORAGE_URL, TOKEN_ID)
+        LoadModelManager.model = obj_service.load_model(CONTAINER_NAME, OBJECT_NAME)
 
-# RabbitMQ 연결 정보 환경 변수로 설정
-rabbitmq_user = os.environ.get('RABBITMQ_USER')
-rabbitmq_password = os.environ.get('RABBITMQ_PASSWORD')
-rabbitmq_host = os.environ.get('RABBITMQ_HOST')
-
-credentials = pika.PlainCredentials(rabbitmq_user, rabbitmq_password)
-connection = pika.BlockingConnection(pika.ConnectionParameters(rabbitmq_host, credentials=credentials))
+# RabbitMQ 연결
+credentials = pika.PlainCredentials(RABBITMQ_USER, RABBITMQ_PASSWORD)
+connection = pika.BlockingConnection(pika.ConnectionParameters(RABBITMQ_HOST, credentials=credentials))
 channel = connection.channel()
-
-channel.queue_declare(queue='txt.predict.queue', durable=True)
+channel.queue_declare(queue='test', durable=True)
 print("RabbitMQ에 연결됨")
 
-#큐에서 메시지 소비할 때 호출되는 콜백 함수
 def callback(ch, method, properties, body):
     """
-    큐로부터 받은 메시지 처리, 로드된 모델로 예측
+    큐로부터 받은 메시지 처리, 로드된 모델 사용해서 예측
     """
+    
     message = body.decode('utf-8')
     data = json.loads(message)
     
@@ -122,7 +129,7 @@ def callback(ch, method, properties, body):
     humidity_message = data.get("outdoorHumidity", {}).get("value")
     total_people_count_message = data.get("totalPeopleCount", {}).get("value")
 
-    #unix_time > local_time_minute으로 변경
+    #unix_time > local_time_minute으로 변경(00시 기준)
     time_object = datetime.fromtimestamp(date_time / 1000)
     time = time_object.hour * 60 + time_object.minute
 
@@ -135,7 +142,7 @@ def callback(ch, method, properties, body):
         'time_in_minutes' : [time]
     })
 
-    predictions = model.predict(data)
+    predictions = LoadModelManager.model.predict(data)
 
       # 0,1 결과를 언어로 변환
     if predictions[0] == 0:
@@ -149,17 +156,17 @@ def callback(ch, method, properties, body):
     channel.basic_publish(exchange='predict_result', routing_key='predict', body=result)
 
 # 콜백 함수를 채널에 등록
-channel.basic_consume(queue='txt.predict.queue', on_message_callback=callback, auto_ack=True)
+channel.basic_consume(queue='test', on_message_callback=callback, auto_ack=True)
 
 # 메시지 소비 시작
 print(' [*] Waiting for messages. To exit press CTRL+C')
 channel.start_consuming()
 
-#프로그램 시작 모델 로드
-model = load_model_job()
+#프로그램 시작시 모델 로드
+LoadModelManager.load_model_job()
 
-# 모델 로드 작업 스케줄링 (매일 자정에 실행)
-schedule.every().day.at("00:00").do(load_model_job)
+# 모델 로드 작업 스케줄링 (매일 00시 30분에 실행)
+schedule.every().day.at("00:30").do(LoadModelManager.load_model_job)
 
 while True:
     # 스케줄링된 작업 실행
