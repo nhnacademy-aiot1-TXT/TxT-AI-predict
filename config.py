7650efb085292e3f43beeb717a8b67aee4df8e94
{joohyeon98} <{ljh221803@gmail.com}>
2024-05-28 16:29:18 +0900

    ERROR : 환경 변수 설정 수정

--- config.py
+++ config.py
@@ -2,19 +2,19 @@ import os
 
 # 환경 변수 설정
 #RABBITMQ 설정
-RABBITMQ_USER = os.environ.get('RABBITMQ_USER')
-RABBITMQ_PASSWORD = os.environ.get('RABBITMQ_PASSWORD')
-RABBITMQ_HOST = os.environ.get('RABBITMQ_HOST')
-RABBITMQ_CONSUME_QUEUE = os.environ.get('RABBITMQ_CONSUME_QUEUE')
+RABBITMQ_USER = os.getenv('RABBITMQ_USER')
+RABBITMQ_PASSWORD = os.getenv('RABBITMQ_PASSWORD')
+RABBITMQ_HOST = os.getenv('RABBITMQ_HOST')
+RABBITMQ_CONSUME_QUEUE = os.getenv('RABBITMQ_CONSUME_QUEUE')
 
 #REDIS 설정
-REDIS_HOST = os.environ.get('REDIS_HOST')
-REDIS_PORT = os.environ.get('REDIS_PORT')
-REDIS_PASSWORD = os.environ.get('REDIS_PASSWORD')
+REDIS_HOST = os.getenv('REDIS_HOST')
+REDIS_PORT = os.getenv('REDIS_PORT')
+REDIS_PASSWORD = os.getenv('REDIS_PASSWORD')
 
 #인증 및 MODEL_LOAD 설정
-TENANT_ID = os.environ.get('TENANT_ID')
-USERNAME = os.environ.get('USERNAME')
-PASSWORD = os.environ.get('PASSWORD') 
-STORAGE_URL = os.environ.get('STORAGE_URL')
+TENANT_ID = os.getenv('TENANT_ID')
+USERNAME = os.getenv('USERNAME')
+PASSWORD = os.getenv('PASSWORD') 
+STORAGE_URL = os.getenv('STORAGE_URL')
 AUTH_URL = 'https://api-identity-infrastructure.nhncloudservice.com/v2.0'
--- model.py
+++ model.py
@@ -11,10 +11,8 @@
     main: 메인 비동기 함수로, 모델을 주기적으로 로드합니다.
 """
 
-import os
 import io
 import joblib
-import logging
 import aiohttp
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
 from config import TENANT_ID, USERNAME, PASSWORD, STORAGE_URL, AUTH_URL
@@ -23,28 +21,6 @@ import nest_asyncio
 nest_asyncio.apply()
 
 CONTAINER_NAME = 'TxT-model'
-
-#콘솔 로그 생성 및 설정
-logger = logging.getLogger()
-logger.setLevel(logging.INFO)
-formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
-console_handler = logging.StreamHandler()
-console_handler.setFormatter(formatter)
-logger.addHandler(console_handler)
-
-# 환경 변수 로딩
-TENANT_ID = os.environ.get('TENANT_ID')
-USERNAME = os.environ.get('USERNAME')
-PASSWORD = os.environ.get('PASSWORD') 
-STORAGE_URL = os.environ.get('STORAGE_URL')
-AUTH_URL = 'https://api-identity-infrastructure.nhncloudservice.com/v2.0'
-
-# 환경 변수 출력
-logging.basicConfig(level=logging.INFO)
-logging.info(f'TENANT_ID: {TENANT_ID}')
-logging.info(f'USERNAME: {USERNAME}')
-logging.info(f'PASSWORD: {PASSWORD}')
-logging.info(f'STORAGE_URL: {STORAGE_URL}')
 
 async def get_token():
     """
@@ -66,7 +42,7 @@ async def get_token():
     async with aiohttp.ClientSession() as session:
         async with session.post(token_url, json=req_body) as response:
             response.raise_for_status()
-            return await response.json()    
+            return await response.json()
 
 async def get_object_list(token_id, container_name):
     """
